fef00330c33eb6f3372bb1293cd3fbc4231d8f02
Xander Miller <xander.miller@gmail.com>
2013-09-16 10:29:59 -0400

    First commit and README update.

---
+++ README.md
@@ -0,0 +1,6 @@
+# Bloccit
+
+A Reddit-style application to demonstrate foundation programming and web development skills.
+
+Made with my mentor at [Bloc](http://bloc.io)
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bloccit::Application.config.secret_token = '96be55f58ce04f90303910323c3a9a79257a588286b251c632c3fbda714ce4344ef209800a2231e50e46789de93bab7fe9b76c36d0a48601e05056e682a87c41'
