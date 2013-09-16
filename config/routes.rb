5d5ebffab2a2a44227462b7719b341a033c2775b
Xander Miller <xander.miller@gmail.com>
2013-09-16 16:21:56 -0400

    Week 3 - Checkpoint: CRUD. - Created Post Resource - Update Index and Show views

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Bloccit::Application.routes.draw do
+  
+  resources :posts
+
   get "welcome/index"
 
-  get "welcome/about"
+  match "about" => 'welcome#about', via: :get
 
   root to: 'welcome#index'
 end
