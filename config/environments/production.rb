cdedc9404d340d6789cf49d30edf0001900a8b6a
Xander Miller <xander.miller@gmail.com>
2013-10-01 18:47:03 -0400

    Week 4 - Checkpoint: Interlude + Added Mailer Credentials to Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,6 @@ Bloccit::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  config.action_mailer.default_url_options = { :host => 'agile-anchorage-9405.herokuapp.com' }  
+end
 end
