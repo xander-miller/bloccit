1ed014fe8ee66c3a6bafaf8cf01e30065296a6d1
Xander Miller <xander.miller@gmail.com>
2013-10-01 16:32:03 -0400

    Week 4 - Checkpoint: Topics and Posts + Added Topics Model + Added Topics Migrations + Created Association between Posts and Topics + Added Topic Seed Data + Nested Post Routes in Topics + Created CRUD Post Views + Refactored Post Controller + Refactored Post Views + Added a User for each authorization level for testing.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Bloccit::Application.routes.draw do
-  
+
   devise_for :users
 
-  resources :posts
-
-  get "welcome/index"
+  resources :topics do
+    resources :posts, except: [:index]
+  end
 
   match "about" => 'welcome#about', via: :get
 
-  root to: 'welcome#index'
+  root :to => 'welcome#index'
 end
