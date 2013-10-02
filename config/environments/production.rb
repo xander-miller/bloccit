38f955ed2d767b0c7918de52515995b9302e4e9a
Xander Miller <xander.miller@gmail.com>
2013-10-01 20:01:59 -0400

    tweaked typo in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,4 +66,4 @@ Bloccit::Application.configure do
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
   config.action_mailer.default_url_options = { :host => 'agile-anchorage-9405.herokuapp.com' }  
 end
-end
+
