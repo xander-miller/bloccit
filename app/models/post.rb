ba868acdb485e8d4bf59171b9baa406de10ce0c6
Xander Miller <xander.miller@gmail.com>
2013-10-07 17:00:57 -0400

    Week 5 - Checkpoint: Uploading Images + Installed Carrierwave + Installed MiniMagik + Added Avatar to User Model + Generated Uploader + Installed Fog + Installed Figaro + Moved S3 and SendGrid Credentials to Figaro Applications.yml + Modifed Views to display Avatars + Added Images to Post Model + Generated Uploader for Post Images + Modifed Views to Display Images with Posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,11 @@
 class Post < ActiveRecord::Base
-  attr_accessible :body, :title, :topic
+  attr_accessible :body, :title, :topic, :image
   has_many :comments
   belongs_to :user
   belongs_to :topic
 
   default_scope order('created_at DESC')
+  mount_uploader :image, ImageUploader # add this line.
 
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131003182329) do
+ActiveRecord::Schema.define(:version => 20131007185738) do
 
   create_table "comments", :force => true do |t|
     t.text     "body"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(:version => 20131003182329) do
     t.datetime "updated_at", :null => false
     t.integer  "user_id"
     t.integer  "topic_id"
+    t.string   "image"
   end
 
   add_index "posts", ["topic_id"], :name => "index_posts_on_topic_id"
