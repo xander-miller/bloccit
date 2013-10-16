678fca8ceec19856962add3c4e8b488a1bfa4b62
Xander Miller <xander.miller@gmail.com>
2013-10-16 16:13:42 -0400

    Week 6 - Checkpoint: Privacy + Added visable scope to Topic model using Lambda + Made show controller only show topics visable to that user + Added authorization to view private topics and posts to members + Added general authorization to view public posts

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -11,6 +11,7 @@ class Ability
       can :manage, Comment, :user_id => user.id
       can :manage, Favorite, user_id: user.id
       can :create, Vote
+      can :read, Topic
     end
 
     # Moderators can delete any post
@@ -24,6 +25,7 @@ class Ability
       can :manage, :all
     end
 
-    can :read, :all
+    can :read, Topic, public: true
+    can :read, Post
   end
 end
