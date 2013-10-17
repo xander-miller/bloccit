e5318bc640e1bd4aa17e2e66bea8734492d922cb
Xander Miller <xander.miller@gmail.com>
2013-10-17 13:32:36 -0400

    Week 6 - Checkpoint: Popular Posts + Moved most of existing posts controller files to topics/posts controller + created new root posts controller to handle popular posts + changed existing post routes to topics/posts + created new post show routes for popular posts + created new post index display + created post partial + Added popular post view to navbar + Created user index to display active users + Added rating algorith for displaying users to user model + Added Users index view to navbar + Created active users view + Used eager loading to increase query performance

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,8 @@ class UsersController < ApplicationController
     @posts = @user.posts.visible_to(current_user) 
   end
 
+  def index
+    @users = User.top_rated.paginate(page: params[:page], per_page: 10)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Bloccit::Application.routes.draw do
 
+  get "posts/index"
+
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: 'users/registrations' }
   resources :users, only: [:show] # for public profiles
+  resources :posts, only: [:index] # for popular posts list
+  resources :users, only: [:show, :index] # Users list
 
   resources :topics do
-    resources :posts, except: [:index] do
+    resources :posts, except: [:index], controller: 'topics/posts' do
       resources :comments, only: [:create, :destroy]
       resources :favorites, only: [:create, :destroy]
       match '/up-vote', to: 'votes#up_vote', as: :up_vote
